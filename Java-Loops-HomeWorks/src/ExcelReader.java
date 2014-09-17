60ef3cb1635f5b28ba043846610115a1e01e3300
d0ntth1nc <d0ntth1nc@gmail.com>
2014-09-17 16:51:40 +0300

    init

--- Java-Loops-HomeWorks/src/ExcelReader.java
+++ Java-Loops-HomeWorks/src/ExcelReader.java
@@ -23,7 +23,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelReader {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO : u need xmlbeans to run this app
 		readSheetWithFormula();
 	}
 	
