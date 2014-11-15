48348e49fb1b667ea932846011b4ca3ce29e6794
d0ntth1nc <d0ntth1nc@gmail.com>
2014-11-15 17:32:10 +0200

    fixed output

--- JavaScript-FunctionsAndObjects/Exam/priceTrends.js
+++ JavaScript-FunctionsAndObjects/Exam/priceTrends.js
@@ -2,19 +2,17 @@ function printPriceTrends(input) {
     console.log("<table>");
     console.log("<tr><th>Price</th><th>Trend</th></tr>");
 
-    var lastPrice = Number(input[0]);
+    var lastPrice = parseFloat(parseFloat(input[0]).toFixed(2));
     for (var i = 0, len = input.length; i < len; i++) {
-        var currentPrice = Number(input[i]);
-
+        var currentPrice = parseFloat(parseFloat(input[i]).toFixed(2));
         var tableRow = "<tr><td>" + currentPrice.toFixed(2) + "</td><td><img src=\"";
         if (lastPrice < currentPrice) {
-            tableRow += "up.png" + "\"/></td></tr>";
+            tableRow += "up.png" + "\"/></td></td>";
         } else if (lastPrice > currentPrice) {
-            tableRow += "down.png" + "\"/></td></tr>";
-        } else {
-            tableRow += "fixed.png" + "\"/></td></tr>";
+            tableRow += "down.png" + "\"/></td></td>";
+        } else if (lastPrice == currentPrice) {
+            tableRow += "fixed.png" + "\"/></td></td>";
         }
-
         lastPrice = currentPrice;
         console.log(tableRow);
     }
