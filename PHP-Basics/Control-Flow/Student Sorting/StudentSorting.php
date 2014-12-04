e1a2ac656820c4e6eed93eba86bc74c8e698cf8f
d0ntth1nc <d0ntth1nc@gmail.com>
2014-12-04 18:34:37 +0200

    formatted

--- PHP-Basics/Control-Flow/Student Sorting/StudentSorting.php
+++ PHP-Basics/Control-Flow/Student Sorting/StudentSorting.php
@@ -14,33 +14,31 @@ include_once( 'form.html' );
 if ($studentsSorter->studentsCount() > 0):
     $students = $studentsSorter->sort( $_POST[ 'orderBy' ], $_POST[ 'order' ] );
     $totalScore = 0;
-?>
-<table>
-<thead>
-<tr>
-    <th>First name:</th>
-    <th>Second name:</th>
-    <th>Email:</th>
-    <th>Exam score:</th>
-</tr>
-</thead>
-    <tbody>
-    <?php foreach ($students as $student): $totalScore += $student[ 'grade' ]?>
-        <tr>
-            <td><?=htmlentities( $student[ 'firstName' ] )?></td>
-            <td><?=htmlentities( $student[ 'lastName' ] )?></td>
-            <td><?=htmlentities( $student[ 'email' ] )?></td>
-            <td><?=htmlentities( $student[ 'grade' ] )?></td>
-        </tr>
-    <?php endforeach; ?>
+    ?>
+    <table>
+    <thead>
     <tr>
-        <td colspan="3">Average Score:</td>
-        <td><?=round( $totalScore / $studentsSorter->studentsCount() )?></td>
+        <th>First name:</th>
+        <th>Second name:</th>
+        <th>Email:</th>
+        <th>Exam score:</th>
     </tr>
-    </tbody>
-</table>
-
-
+    </thead>
+        <tbody>
+        <?php foreach ($students as $student): $totalScore += $student[ 'grade' ]?>
+            <tr>
+                <td><?=htmlentities( $student[ 'firstName' ] )?></td>
+                <td><?=htmlentities( $student[ 'lastName' ] )?></td>
+                <td><?=htmlentities( $student[ 'email' ] )?></td>
+                <td><?=htmlentities( $student[ 'grade' ] )?></td>
+            </tr>
+        <?php endforeach; ?>
+        <tr>
+            <td colspan="3">Average Score:</td>
+            <td><?=round( $totalScore / $studentsSorter->studentsCount() )?></td>
+        </tr>
+        </tbody>
+    </table>
 <?php endif; ?>
 <script src="script.js"></script>
 </body>
