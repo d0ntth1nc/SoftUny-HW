92afad58b1a4688362e95c452f6305d34ab0dd10
Alexandur Seferinkyn <d0ntth1nc@gmail.com>
2015-02-01 16:24:22 +0200

    .

--- HighQualityCode/02. Code-Formatting-Homework1/Messages.cs
+++ HighQualityCode/02. Code-Formatting-Homework1/Messages.cs
@@ -6,43 +6,43 @@ using Wintellect.PowerCollections;
 
 internal static class Messages
 {
-	private static StringBuilder output = new StringBuilder();
+    private static StringBuilder output = new StringBuilder();
 
-	public static string Output
-	{
-		get
-		{
-			return output.ToString();
-		}
-	}
+    public static string Output
+    {
+        get
+        {
+            return output.ToString();
+        }
+    }
 
-	public static void EventAdded()
-	{
-		output.AppendLine("Event added");
-	}
+    public static void EventAdded()
+    {
+        output.AppendLine("Event added");
+    }
 
-	public static void EventDeleted(int x)
-	{
-		if (x == 0)
-		{
-			NoEventsFound();
-		}
-		else
-		{
-			output.AppendFormat("{0} events deleted\n", x);
-		}
-	}
+    public static void EventDeleted(int x)
+    {
+        if (x == 0)
+        {
+            NoEventsFound();
+        }
+        else
+        {
+            output.AppendFormat("{0} events deleted\n", x);
+        }
+    }
 
-	public static void NoEventsFound()
-	{
-		output.AppendLine("No events found");
-	}
+    public static void NoEventsFound()
+    {
+        output.AppendLine("No events found");
+    }
 
-	public static void PrintEvent(Event eventToPrint)
-	{
-		if (eventToPrint != null)
-		{
-			output.AppendLine(eventToPrint);
-		}
-	}
+    public static void PrintEvent(Event eventToPrint)
+    {
+        if (eventToPrint != null)
+        {
+            output.AppendLine(eventToPrint);
+        }
+    }
 }
