626cad0632cae1f2f7aa5479d900df8898635eb7
d0ntth1nc <d0ntth1nc@gmail.com>
2015-05-09 12:39:05 +0300

    first angular homework

---
+++ AngularJSHomeworks/Students Page/app.js
@@ -0,0 +1,15 @@
+(function(){
+angular
+	.module( "StudentsPage", [] )
+	.controller( "StudentsController", function( $scope ) {
+		var model = {
+			"name": "Pesho",
+			"photo": "http://www.nakov.com/wp-content/uploads/2014/05/SoftUni-Logo.png",
+			"grade": 5,
+			"school": "High School of Mathematics",
+			"teacher": "Gichka Pesheva",
+		};
+		
+		angular.extend( $scope, model );
+	});
+}())
